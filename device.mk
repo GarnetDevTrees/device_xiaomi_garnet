d233aaa64e12e80fc8ff9a2d707d95f0e7712ba5
SebaUbuntu <seba@sebaubuntu.dev>
2024-06-15 13:21:08 +0700

    garnet: Setup TARGET_HAS_UDFPS flag
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- device.mk
+++ device.mk
@@ -203,6 +203,8 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+TARGET_HAS_UDFPS := true
+
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi \
     libudfpshandler \
