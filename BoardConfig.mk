7a4a0b1cc44171a998d2c4d11a391d3ee993714e
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-04-20 22:25:00 +0300

    Revert "garnet: Add Dolby support, adapt inline repo, add audio effects"
    This reverts commit 26a6159b1b895f240a44642e3570eb6fe5dc62ab.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,13 +72,13 @@ TARGET_GLOBAL_OPTIMIZATION := O3
 TARGET_GLOBAL_THINLTO := true
 
 # HIDL
-DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
-DEVICE_MANIFEST_FILE += \
+DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/configs/hidl/manifest.xml \
     hardware/qcom-caf/sm8450/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
 
