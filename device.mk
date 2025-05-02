a91e667485a424fadcc86d5af1948d079a8f49b6
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-05-02 19:05:08 +0300

    garnet: Remove support for Moto Dolby Atmos
    This reverts commit 09571765ea684ca44da3f2adbc99413cf07cf894.
    
    Revert "garnet: Audio Policy: Import Bluetooth Devices Encodings for Dolby Processing"
    
    This reverts commit 25deed766c0c9507a91d0d3c6ab3804984cba100.
    
    Revert "garnet: audio: Add dolby volume listener"
    
    This reverts commit 38a4a16af7f202d87c23a933b0731e4dbcb6a480.
    
    Revert "garnet: audio: Sync with MIUI CN 14 dolby effects"
    
    This reverts commit 3eedfa0154ea98ebb4c36fafaf895a9039e67cc3.
    
    Revert "garnet: Include Moto Dolby Atmos"
    
    This reverts commit 41436707c1693bae6948e4d8e79960e537047a4d.

--- device.mk
+++ device.mk
@@ -10,9 +10,6 @@ $(call inherit-product-if-exists, device/xiaomi/miuicamera-garnet/device.mk)
 # Inherit Basic Call Recorder (BCR)
 $(call inherit-product-if-exists, vendor/bcr/bcr.mk)
 
-# Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
-
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
