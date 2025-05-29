8f36e783385acaeb96f5f27910d99da19a049af5
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-05-29 21:56:13 +0300

    garnet: Drop build keys

--- device.mk
+++ device.mk
@@ -608,7 +608,5 @@ PRODUCT_PACKAGES += \
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
--include vendor/voltage-priv/keys/keys.mk
-
 # Viper4AndroidFX
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
