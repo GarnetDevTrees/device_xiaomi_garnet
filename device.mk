78ce756ddcdb6cbee994b0d39b3c370fb60b0b82
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-06-25 21:26:34 +0300

    Revert "garnet: Import QTI power HAl"
    This reverts commit 232b4fb5f46ebe18bf19a13c1244631888db93a9.

--- device.mk
+++ device.mk
@@ -439,7 +439,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti-sm7435
+    android.hardware.power-service-qti
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3.vendor
