db1544bae18f96c79f1f58f47386f3cf644ba19f
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-02 20:00:30 +0300

    garnet: Include LiteGapps in the build Signed-off-by: Sensei <senseidev23@gmail.com>

--- device.mk
+++ device.mk
@@ -614,5 +614,8 @@ $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
 -include vendor/voltage-priv/keys/keys.mk
 
+# LiteGApps
+-include vendor/gapps/arm64/arm64-vendor.mk
+
 # Viper4AndroidFX
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
