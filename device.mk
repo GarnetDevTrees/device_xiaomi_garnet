8b320f3d6fce001b053a65e15b72f97758f290de
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-20 20:34:01 +0300

    garnet: Drop support for ViPER4AndroidFX
    This reverts commit a2a227d0931916c1fdea28859815bf02621d1941.

--- device.mk
+++ device.mk
@@ -613,6 +613,3 @@ $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
 # LiteGApps
 -include vendor/gapps/arm64/arm64-vendor.mk
-
-# Viper4AndroidFX
-$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
