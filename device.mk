f32d26a6c99d5197e6ca5f9c038ee86a9bc8f6c0
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-09-02 07:08:25 +0300

    Revert "garnet: Switch to libperfmgr"
    This reverts commit 7a6e9214f7416961bed19227b199cc883614862c.

--- device.mk
+++ device.mk
@@ -465,11 +465,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -520,10 +516,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
 # Telephony
